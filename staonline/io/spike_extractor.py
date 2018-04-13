90388f9fcbf339e1dd321dd72fb8a06506a6a92a
Chris Wilson <christhewilson@gmail.com>
2018-04-13 10:35:56 -0400

    WIP spike extraction and storage stuff

--- staonline/io/spike_extractor.py
+++ staonline/io/spike_extractor.py
@@ -1,33 +1,70 @@
 import numpy as np
 from scipy import signal
 from concurrent import futures
+import staonline.io.storage as storage
+
 
 class SpikeExtractor:
+    """
+    Extracts spikes and frametimes from a datastore.
+    """
+    def __init__(self, data_source:storage.DatReader, spike_threshold=3., chs_neural=range(64), ch_frames=73, n_threads=6):
+        """
+        :param data_source: Data source object with a get_next() method
+        :param spike_threshold:
+        :param chs_neural:
+        :param ch_frames:
+        :param n_threads: number of threads to run processing in. (default 6)
+        """
 
-    def __init__(self, storage):
-        self.storage = storage
-        self.executor = futures.ThreadPoolExecutor()
+        self.storage = data_source
+        self.executor = futures.ThreadPoolExecutor(n_threads)
+        self.threshold = spike_threshold
+
+        # todo: generate neural channels here - not all channels should be spike extracted.
+        self.chs_neural = chs_neural
+        self.ch_frame_trig = ch_frames
 
     def get_next(self):
+        """
+        Gets data from data source and extracts spiketimes and frametimes relative to the start of the
+
+        :return: tuples of spiketimes, and frametimes.
+        """
         data = self.storage.get_next()
+        if data is None:
+            return None
+
         p_data = np.full(data.shape, True, dtype=bool)
-        my_futures = {self.executor.submit(process_channel, data[x, :]): x for x in range(self.storage.nch)}
+        my_futures = {self.executor.submit(self.process_channel, data[x, :] ): x for x in self.chs_neural}
+        # todo: my_futures[self.executor.submit(self.get_frametimes)] = 'FLAG'
         # Store processed channels in order of completion
         for future in futures.as_completed(my_futures):
             ch = my_futures[future]
-            try:
-                tmp = future.result()
-            except Exception as exc:
-                print('Row %r generated an exception: %s' % (ch, exc))
+            if not ch == 'FLAG':
+
+                try:
+                    tmp = future.result()
+                except Exception as exc:
+                    print('Row %r generated an exception: %s' % (ch, exc))
+                else:
+                    p_data[ch, :] = tmp
             else:
-                p_data[ch, :] = tmp
+                frametimes = future.result()
         mua_data = np.logical_or.reduce(p_data)
         spk_times = np.convolve(mua_data, np.array([1, -1]), mode='full')
         spk_times = np.where(spk_times == 1)[0]
-        return spk_times
+        return spk_times, frametimes
 
     @staticmethod
-    def process_channel(channel, fs=30000):
+    def process_channel(channel, threshold=3.,  fs=30000):
+        """
+
+        :param channel:
+        :param threshold:
+        :param fs:
+        :return:
+        """
         nband = [x * 1 / (fs / 2) for x in [300, 800]]
         b, a = signal.butter(4, nband, btype='band')
         channel = signal.filtfilt(b, a, channel)
@@ -35,3 +72,16 @@ class SpikeExtractor:
         sd = np.std(channel)
         out = channel > mu + 3 * sd
         return out
+
+    def get_frametimes(self):
+        """
+
+        :return: array of integers
+        """
+
+        #TODO
+
+        # be careful if we end high on last data pull, so that we don't double-count
+        # a frametrigger!!!
+        pass
+
--- staonline/io/storage.py
+++ staonline/io/storage.py
@@ -1,25 +1,63 @@
 import numpy as np
-from os.path import getsize
+import os
 
 
-class Storage:
-    def __init__(self, filename, nch=75, fs=30000, nsec=10):
-        self.data = np.array([])
+class DatReader:
+    """
+    Reads data incrementally from flat binary file (dat).
+    """
+    def __init__(self, filename, nch=75, fs=30000, max_read_secs=10):
+        """
+        :param filename:
+        :param nch: TOTAL number of channels in the array
+        :param fs: default 30000 khz
+        :param max_read_secs: maximum amount of data to return in seconds.
+        """
+
         self.filename = filename  # File to read from
         self.pos = 0              # Position in file
         self.nch = nch            # Number of recording channels
         self.fs = fs              # Sampling frequency
-        self.nsec = nsec          # Max amount of data to load at a time (in seconds)
+        self.max_secs = max_read_secs          # Max amount of data to load at a time (in seconds)
+        self.min_secs = 1  # minimum number of seconds that we will read
+
+        self._bytes_per_sample = 2 # datatype is int16
+        self._max_read_samples = self.max_secs * fs * nch
+        self._max_read_bytes = self._max_read_samples * 2
+        self._bytes_per_row = self._bytes_per_sample * self.nch
+        min_read_samples = self.min_secs * fs * nch
+        self._min_read_bytes = min_read_samples * self._bytes_per_sample
 
     def get_next(self):
+        """
+        returns the next data array from the file.
+        :return:
+        """
+
+        # calculate how much new data we have in bytes and verify that we have at least the minimum amount of
+        # data, otherwise return None.
+        current_file_len = os.path.getsize(self.filename)  # Update position in file...
+        current_file_len_full_rows = current_file_len - current_file_len % self._bytes_per_row
+        # ^ modulo makes sure we're reading a whole row so we can reshape.
+        new_bytes = current_file_len_full_rows - self.pos
+        if new_bytes < self._min_read_bytes:
+            return None
+
+        # We're going to read the next 10 seconds or whatever data is available in the file:
+        read_to_pos = min(
+            (current_file_len_full_rows,
+             self._max_read_bytes + self.pos)
+        )
+
+        assert not read_to_pos % self._bytes_per_sample
+        read_count_values = read_to_pos // self._bytes_per_sample  # number of values to read into numpy.
+
         with open(self.filename, 'rb') as fr:
-            fr.seek(self.pos)  # Move to current position
-            self.pos = getsize(self.filename)  # Update position in file...
-            # Load a maximum of self.nsec seconds of data
-            if self.pos - fr.tell() < 2 * self.nsec * self.fs * self.nch:
-                self.data = np.fromfile(fr, dtype='int16')
-            else:
-                self.data = np.fromfile(fr, count=self.nsec * self.fs * self.nch, dtype='int16')
-            self.data = np.reshape(self.data, [self.nch, int(self.data.size / self.nch)], order='F')
-        fr.close()
-        return self.data
+            fr.seek(self.pos)
+            data = np.fromfile(fr, count=read_count_values, dtype='int16')
+
+        data = np.reshape(data, [self.nch, data.size // self.nch], order='F')  # why F?
+
+        # TODO: Roman, plz check whether we should seek to the last byte we read or to the next byte so we don't double read
+        self.pos = read_to_pos  # this is where we'll seek in the next read.
+        return data
